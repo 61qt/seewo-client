9d78ecbe0ba2b55dd23f8b9f5fc98a5bfc12ea15
fanwei <fanwei@gdzhsng.com>
2022-12-05 18:48:07 +0800

    获取区域看板统计数据

---
+++ src/SchoolOpen/Client.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace QT\Seewo\SchoolOpen;
+
+use QT\Seewo\BaseClient;
+use GuzzleHttp\Psr7\Response;
+
+class Client extends BaseClient
+{
+    /**
+     * 调用接口凭证
+     *
+     * @var string
+     */
+    protected $ticketId;
+
+    /**
+     * @param string $appid
+     * @param string $secret
+     * @param string $ticketId
+     * @param array $options
+     */
+    public function __construct(string $appid, string $secret, string $ticketId, array $options = [])
+    {
+        $this->ticketId = $ticketId;
+
+        parent::__construct($appid, $secret, $options);
+    }
+
+    /**
+     * 获取区域看板统计数据
+     *
+     * https://open.seewo.com/#/service/1423/doc/1795
+     * @param int $areaId
+     * @return Response
+     */
+    public function getAreaRankingStatistics(int $areaId): Response
+    {
+        return $this->post('/school-open/area-ranking/statistics', [
+            'areaId'   => $areaId,
+            "ticketId" => $this->ticketId,
+        ]);
+    }
+}
